0f842ee61f74e53247ddea352a0430024758c6e1
Jarapala-Baba <babajarapala@gmail.com>
2021-04-07 10:30:40 +0530

    JavaScript_day_1
    Java Script Day 1 Learnings

---
+++ index.js
@@ -0,0 +1,15 @@
+let name='Baba Jarapala'; //String literal
+let age= 24;              // Number Literal
+let isApproved=true;      // Boolean Literal
+let firstName=undefined;
+let lastName=null;
+console.log(name)
+
+// Rules
+// Cannot be a reserved variables
+// should be Meaningful
+// Caqnnot start with a number
+// They Cannot COntain Space or hyphen
+// Convension used is JavaScript is Camel Caseing
+// Are case-sensitive
+
